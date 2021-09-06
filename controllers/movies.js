6845629e0e8807926d9f57732a622f9848d3ce3a
Daniel Badura <daniel.badura@outlook.com>
2021-09-06 02:09:42 +0200

    added movies show page

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,17 +1,29 @@
 //jshint esversion:9
-const axios = require('axios')
-function fetchMovie(title){
-   return axios
-    //   .get(`http://www.omdbapi.com/?apikey=f7196b6c&t=${title}`)
-      .get(`http://www.omdbapi.com/?apikey=f7196b6c&s=${title}`)
-      .then((response) => response.data)
-      .catch((err) => "error")};
+const axios = require("axios");
+function fetchMovies(title) {
+  return axios
+    .get(`http://www.omdbapi.com/?apikey=f7196b6c&s=${title}`)
+    .then((response) => response.data)
+    .catch((err) => "error");
+}
+function fetchMovie(title) {
+  return axios
+    .get(`http://www.omdbapi.com/?apikey=f7196b6c&t=${title}`)
+    .then((response) => response.data)
+    .catch((err) => "error");
+}
 
 module.exports.showMovies = async (req, res) => {
-    const  search  = req.body.search;
-  let movies = await fetchMovie(search);
-  res.render("movies/search", {movies});
-//   res.send(movies);
+  const search = req.body.search;
+  let movies = await fetchMovies(search);
+  res.render("movies/search", { movies, search });
+  //   res.send(movies);
 };
-module.exports.searchMovies = async (req, res) => {res.render("movies/index", {});
+module.exports.searchMovies = async (req, res) => {
+  res.render("movies/index", {});
+};
+module.exports.showMovie = async (req, res) => {
+  const title = req.params.title;
+  let movie = await fetchMovie(title);
+  res.render("movies/show", { movie });
 };
--- routes/movies.js
+++ routes/movies.js
@@ -2,11 +2,11 @@
 const express = require("express");
 const router = express.Router();
 const catchAsync = require("../utils/catchAsync")
-const { showMovies, searchMovies } = require("../controllers/movies");
+const { showMovies, searchMovies,showMovie } = require("../controllers/movies");
 // Movies
 
 // router.post("/show", catchAsync(showMovies));
-router.get("/show", catchAsync(showMovies));
+router.get("/show/:title", catchAsync(showMovie));
 router.post("/show", catchAsync(showMovies));
 router.get("/", catchAsync(searchMovies));
 
