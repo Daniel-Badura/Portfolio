5ceab48f36e9175b0ca88a73a47727fca3285145
Daniel-Badura <daniel.badura@outlook.com>
2021-12-08 22:52:43 +0100

    fixing heroku db issue

--- config/db.js
+++ config/db.js
@@ -2,11 +2,9 @@
 const mongoose = require('mongoose');
 require('dotenv').config({ path: './config/config.env' })
 
-const dburl =
-  process.env.MONGODB_URI || "mongodb://localhost:27017/portfolio";
-const dburl2 = process.env.MONGODB_URI || "mongodb://localhost:27017/portfolio";
+const dburi = process.env.MONGODB_URI;
 const connectDB = async () => {
-  const conn = await mongoose.connect(dburl, {
+  const conn = await mongoose.connect(dburi, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
