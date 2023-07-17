6b8e6ff9cfa579b0626ee4b458106468f7a14b71
Daniel Badura <daniel.badura@outlook.com>
2023-07-17 09:14:36 +0200

    added Testowo link

--- seeds/projects.js
+++ seeds/projects.js
@@ -1,51 +1,58 @@
 module.exports.projects = [
   {
-    "name": "WebSklep",
-    "href": "https://websklep.herokuapp.com/",
-    "repo": "https://github.com/Daniel-Badura/websklep",
-    "description": "Sklep internetowy - MERN stack",
-    "color": "orange"
+    name: "WebSklep",
+    href: "https://websklep.herokuapp.com/",
+    repo: "https://github.com/Daniel-Badura/websklep",
+    description: "Sklep internetowy - MERN stack",
+    color: "orange",
+  },
+  {
+    name: "Testowo",
+    href: "https://testowo-555445a60efa.herokuapp.com/",
+    repo: "https://github.com/Daniel-Badura/testowo",
+    description: "Aplikacja do tworzenia testów online - MERN stack",
+    color: "orange",
   },
 
   {
-    "name": "Github Lens",
-    "href": "https://github-lens.herokuapp.com/",
-    "repo": "https://github.com/Daniel-Badura/Github-Lens",
-    "description": "Wyszukiwarka użytkowników Github - MERN stack",
-    "color": "green"
+    name: "Github Lens",
+    href: "https://github-lens.herokuapp.com/",
+    repo: "https://github.com/Daniel-Badura/Github-Lens",
+    description: "Wyszukiwarka użytkowników Github - MERN stack",
+    color: "green",
   },
   {
-    "name": "Widoczki",
-    "href": "/widoczki",
-    "repo": "https://github.com/Daniel-Badura/Portfolio",
-    "description": "Strona z ładnymi widoczkami - Express/EJS/MongoDB",
-    "color": "teal"
+    name: "Widoczki",
+    href: "/widoczki",
+    repo: "https://github.com/Daniel-Badura/Portfolio",
+    description: "Strona z ładnymi widokami - Express/EJS/MongoDB",
+    color: "teal",
   },
   {
-    "name": "WWFDB",
-    "href": "/movies",
-    "description": "Wyszukiwarka filmowa",
-    "color": "teal"
+    name: "WWFDB",
+    href: "/movies",
+    description: "Wyszukiwarka filmowa",
+    color: "teal",
   },
   {
-    "name": "Keynotes",
-    "href": "/keynotes",
-    "description": "Wirtualne klawisze - Pure JS",
-    "color": "teal"
-  },
-
-  {
-    "name": "Templata",
-    "href": "/template",
-    "description": "Templata strony internetowej",
-    "color": "blue"
+    name: "Keynotes",
+    href: "/keynotes",
+    description: "Wirtualne klawisze - Pure JS",
+    color: "teal",
   },
 
   {
-    "name": "Mytodos",
-    "href": "",
-    "repo": "https://github.com/Daniel-Badura/Mytodoos",
-    "description": "To-do list app",
-    "color": "purple"
-  }
+    name: "Templata",
+    href: "/template",
+    description: "Templata strony internetowej",
+    color: "blue",
+  },
+
+  {
+    name: "Mytodos",
+    href: "",
+    repo: "https://github.com/Daniel-Badura/Mytodoos",
+    description: "To-do list app",
+    color: "purple",
+  },
 ];
